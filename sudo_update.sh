ff7b340bf2e90000b429eff208771a3d988a16c6
mareox <mareox@gmail.com>
2023-03-02 17:17:53 -0800

    Update sudo_update.sh

--- sudo_update.sh
+++ sudo_update.sh
@@ -1,1 +1,1 @@
-sudo apt update -y & sudo apt upgrade -y & sudo apt update -y $ sudo apt clean & sudo apt autoremove $ sudo apt purge
+sudo apt upgrade -y & sudo apt update -y $ sudo apt autoclean & sudo apt autoremove $ sudo apt autopurge
