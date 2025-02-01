b6331e94fe2f961069af6e8e3a3b7289a5ba59c7
mareox <mareox@gmail.com>
2025-02-01 02:41:12 -0800

    Update docker-enable-tcp2375.sh

--- docker-enable-tcp2375.sh
+++ docker-enable-tcp2375.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create the /etc/docker directory if it doesn't exist
-sudo mkdir -p /etc/docker
+mkdir -p /etc/docker
 
 # Create the daemon.json file with the specified configuration
 cat <<EOF > /etc/docker/daemon.json
@@ -11,7 +11,7 @@ cat <<EOF > /etc/docker/daemon.json
 EOF
 
 # Create the systemd override directory if it doesn't exist
-sudo mkdir -p /etc/systemd/system/docker.service.d
+mkdir -p /etc/systemd/system/docker.service.d
 
 # Create the override.conf file with the specified configuration
 cat <<EOF > /etc/systemd/system/docker.service.d/override.conf
@@ -21,9 +21,9 @@ ExecStart=/usr/bin/dockerd
 EOF
 
 # Reload the systemd daemon
-sudo systemctl daemon-reload
+systemctl daemon-reload
 
 # Restart the Docker service
-sudo systemctl restart docker.service
+systemctl restart docker.service
 
 echo "Docker configuration updated and service restarted."
